770528113cd930f57e73538d8537f5c3d409d3b9
kuqmua <serezhashmakov@outlook.com>
2022-10-29 11:37:16 +0300

    InitErrorFromTufaCommon InitErrorFromCrate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-#[proc_macro_derive(InitError)]
-pub fn derive_init_error(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(InitErrorFromTufaCommon)]
+pub fn derive_init_error_from_tufa_common(
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    generate(
+        input,
+        "tufa_common::traits::new_error::NewError",
+        "tufa_common::where_was::WhereWas",
+    )
+}
+
+#[proc_macro_derive(InitErrorFromCrate)]
+pub fn derive_init_error_from_crate(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    generate(
+        input,
+        "tufa_common::traits::new_error::NewError",
+        "tufa_common::where_was::WhereWas",
+    )
+}
+// tufa_common::traits::new_error::NewError
+// tufa_common::where_was::WhereWas
+fn generate(
+    input: proc_macro::TokenStream,
+    new_error_path: &str,
+    where_was_path: &str,
+) -> proc_macro::TokenStream {
     let ast: syn::DeriveInput = syn::parse(input).expect("InitError syn::parse(input) failed");
     let fields = match ast.data {
         syn::Data::Struct(struct_item) => struct_item.fields,
@@ -38,9 +62,11 @@ pub fn derive_init_error(input: proc_macro::TokenStream) -> proc_macro::TokenStr
         // syn::Fields::Unit => todo!(),
         _ => panic!("InitError only works with named fields"),
     };
+    let new_error_path_ident = syn::Ident::new(new_error_path, ident.span());
+    let where_was_path_ident = syn::Ident::new(where_was_path, ident.span());
     let gen = quote::quote! {
-        impl tufa_common::traits::new_error::NewError<#source_type_ident> for #ident {
-            fn new(source: #source_type_ident, where_was: tufa_common::where_was::WhereWas) -> Self {
+        impl #new_error_path_ident<#source_type_ident> for #ident {
+            fn new(source: #source_type_ident, where_was: #where_was_path_ident) -> Self {
                 Self { source, where_was }
             }
         }
